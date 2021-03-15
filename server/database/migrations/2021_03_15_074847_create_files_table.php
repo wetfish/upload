e8c0d4752fc70383e8cbccee54517d0a8602db46
Rachel Fish <itsrachelfish@gmail.com>
2021-03-15 02:00:09 -0600

    Created migrations

---
+++ server/database/migrations/2021_03_15_074847_create_files_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('mime_type');
+            $table->foreignId('gallery_id')->nullable()->constrained();
+            $table->unsignedBigInteger('uploaded_by_user');
+            $table->foreign('uploaded_by_user')->references('id')->on('users');
+            $table->string('uploaded_by_ip');
+            $table->string('file_path', 2048);
+            $table->string('url_path', 2048);
+            $table->string('original_file_name', 2048);
+            $table->string('read_permission');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
