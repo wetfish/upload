6b871cf40257967ef80e56532fd5f34ada865977
Rachel Fish <itsrachelfish@gmail.com>
2021-03-23 01:26:53 -0600

    Created isValid function for challenges

--- server/app/Models/Challenge.php
+++ server/app/Models/Challenge.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use phpseclib3\Crypt\PublicKeyLoader;
+use App\Models\Key;
 
 class Challenge extends Model
 {
-    //
+    // Challenges belong to a specific key
+    public function key()
+    {
+        return $this->belongsTo(Key::class);
+    }
+
+    public function isValid($encodedSignature)
+    {
+        $pubkey = PublicKeyLoader::load($this->key()->pubkey);
+        $signature = base64_decode($encodedSignature);
+
+        if($pubkey->verify($this->string, $signature)) {
+            return true;
+        }
+
+        return false;
+    }
 }
--- server/app/Models/Key.php
+++ server/app/Models/Key.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Challenge;
+use App\Models\User;
 
 class Key extends Model
 {
-    protected $fillable = ['pubkey'];
+    // Keys have many challenges
+    public function challenges()
+    {
+        return $this->hasMany(Challenge::class);
+    }
+
+    // Keys can belong to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
