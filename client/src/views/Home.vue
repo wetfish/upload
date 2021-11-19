2e2c2693b633d149cc1633d87aa0bebda4687563
Rachel Fish <itsrachelfish@gmail.com>
2021-11-19 01:32:04 -0700

    Set up self-signed SSL locally and basic keypair generation

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,7 +5,7 @@
     <h1>Development Todo</h1>
 
     <ul style="width: 250px; margin: 0 auto; text-align: left">
-      <li>Generate / Load a keypair</li>
+      <li><s>Generate / Load a keypair</s></li>
       <li>Test signing arbitrary data</li>
       <li>Sign a challenge from the API</li>
       <li>Actually upload a file!!!</li>
@@ -15,10 +15,28 @@
 </template>
 
 <script>
-
 export default {
   name: 'Home',
   components: {
-  }
+  },
+
+  async created() {
+    console.log("Hi there!");
+
+    const keyPair = await crypto.subtle.generateKey(
+      {
+        name: "RSA-PSS",
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([1, 0, 1]),
+        hash: "SHA-256",
+      },
+
+      false, // Don't allow this key to be exported
+
+      ["sign", "verify"] // Permissions needed by this key
+    );
+
+    console.log(keyPair);
+  },
 }
 </script>
